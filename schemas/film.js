fd2f40dae18ace6726ba2d1294318fb5a3272710
luk-str <lstrociak@gmail.com>
2021-01-12 15:58:57 +0100

    Prepare person schema

--- schemas/film.js
+++ schemas/film.js
@@ -12,10 +12,16 @@ export default {
       type: "string",
     },
     {
+      name: "genre",
+      title: "Genre",
+      type: "string",
+      description: "e.g. Horror, Drama, Short Film",
+    },
+    {
       name: "slug",
       title: "Slug",
       description:
-        "The url of the film on our website. Best to generate automatically",
+        "The url of the film on our website. Best to just click 'Generate'!",
       type: "slug",
       options: {
         source: "title",
@@ -23,15 +29,24 @@ export default {
       },
     },
     {
-      name: "link",
+      name: "videoLink",
       type: "url",
-      title: "Link",
-      description: "Link to the film on youtube/vimeo",
+      title: "YouTube / Vimeo Link",
+      description:
+        "To get a clean link, click on the 'share' button on youtube and make sure that the 'Start at: ' option is turned off.",
+    },
+    {
+      name: "externalLink",
+      type: "url",
+      title: "External Page",
+      description: "Link to the project on IMDb or some other service",
     },
     {
       name: "description",
       title: "Description",
       type: "blockContent",
+      description:
+        "Short description of the project. Don't include credits as these have a separate field.",
     },
     {
       name: "releaseDate",
--- schemas/person.js
+++ schemas/person.js
@@ -1,36 +1,57 @@
-import UserIcon from 'part:@sanity/base/user-icon'
+import UserIcon from "part:@sanity/base/user-icon";
 
 export default {
-  name: 'person',
-  title: 'Person',
-  type: 'document',
+  name: "person",
+  title: "Person",
+  type: "document",
   icon: UserIcon,
   fields: [
     {
-      name: 'name',
-      title: 'Name',
-      type: 'string',
+      name: "name",
+      title: "Name",
+      type: "string",
       description: 'Please use "Firstname Lastname" format',
     },
     {
-      name: 'slug',
-      title: 'Slug',
-      type: 'slug',
+      name: "slug",
+      title: "Slug",
+      type: "slug",
       options: {
-        source: 'name',
+        source: "name",
         maxLength: 100,
       },
+      description:
+        "The url for the person's page on our website. Best to just click 'Generate'!",
     },
     {
-      name: 'image',
-      title: 'Image',
-      type: 'image',
+      name: "role",
+      title: "Role",
+      type: "string",
+      description: "The role of the person in the club (optional)",
+    },
+    {
+      name: "link",
+      title: "Social Link",
+      type: "url",
+      description:
+        "Link to the person's IMDb or some other social page (optional)",
+    },
+    {
+      name: "bio",
+      title: "Bio",
+      type: "blockContent",
+      description: "A short bio (optional)",
+    },
+    {
+      name: "image",
+      title: "Image",
+      type: "image",
       options: {
         hotspot: true,
       },
     },
   ],
   preview: {
-    select: {title: 'name', media: 'image'},
+    select: { title: "name", media: "image" },
   },
-}
+};
