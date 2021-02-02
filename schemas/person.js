18aa8eb469544ff04dbbfce8ad249f2a9113ee67
luk-str <lstrociak@gmail.com>
2021-02-02 20:01:43 +0100

    Add field for website links

--- schemas/person.js
+++ schemas/person.js
@@ -12,36 +12,33 @@ export default {
       type: "string",
       description: 'Please use "Firstname Lastname" format',
     },
+
     {
       name: "slug",
       title: "Slug",
       type: "slug",
+      description:
+        "The url for the person's page on our website. Best to just click 'Generate'!",
       options: {
         source: "name",
         maxLength: 100,
       },
-      description:
-        "The url for the person's page on our website. Best to just click 'Generate'!",
     },
+
     {
       name: "role",
       title: "Role",
       type: "string",
       description: "The role of the person in the club (optional)",
     },
-    {
-      name: "link",
-      title: "Social Link",
-      type: "url",
-      description:
-        "Link to the person's IMDb or some other social page (optional)",
-    },
+
     {
       name: "bio",
       title: "Bio",
       type: "blockContent",
       description: "A short bio (optional)",
     },
+
     {
       name: "image",
       title: "Image",
@@ -50,7 +47,46 @@ export default {
         hotspot: true,
       },
     },
+
+    {
+      name: "links",
+      title: "Links",
+      type: "array",
+      description: "Links to the person's profiles on the internet",
+      of: [
+        {
+          name: "link",
+          title: "Link",
+          type: "object",
+          fields: [
+            {
+              name: "websiteName",
+              title: "Website Name",
+              type: "string",
+              options: {
+                list: [
+                  "imdb",
+                  "youtube",
+                  "facebook",
+                  "twitter",
+                  "instagram",
+                  "spotify",
+                  "linkedin",
+                  "other website",
+                ],
+              },
+            },
+            {
+              name: "url",
+              title: "url",
+              type: "url",
+            },
+          ],
+        },
+      ],
+    },
   ],
+
   preview: {
     select: { title: "name", subtitle: "role", media: "image" },
   },
